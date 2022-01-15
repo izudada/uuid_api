4c595710da75bbb33b3ac4216fcd76ff11ceb996
Anthony <tonyudeagbala@gmail.com>
2022-01-15 20:29:00 +0000

    Create CowrywiseCustomer

--- drf_uuid/models.py
+++ drf_uuid/models.py
@@ -1,3 +1,14 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+class CowrywiseCustomer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()
+    uuid = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
