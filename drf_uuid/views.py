cc91eda5b08c85405e9475b1701b31e5c3f9b719
Anthony <tonyudeagbala@gmail.com>
2022-01-16 10:25:00 +0000

    Create post method

--- drf_uuid/views.py
+++ drf_uuid/views.py
@@ -4,3 +4,11 @@ from rest_framework.response import Response
 
 from .serializers import CustomerSerializer
 from .models import CowrywiseCustomer
+
+class CowrywiseAPI(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            uuid = ""
+            serializer.save(uuid=uuid)
+            return Response(serializer.data)
