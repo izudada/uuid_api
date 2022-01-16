303e9a75dcb433b9281fb22587ef639e885ca8fc
Anthony <tonyudeagbala@gmail.com>
2022-01-16 10:46:53 +0000

    Add uuid feature

--- drf_uuid/views.py
+++ drf_uuid/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import random, string
 
 from .serializers import CustomerSerializer
 from .models import CowrywiseCustomer
@@ -9,6 +10,10 @@ class CowrywiseAPI(APIView):
     def post(self, request, *args, **kwargs):
         serializer = CustomerSerializer(data=request.data)
         if serializer.is_valid():
+            letters_numbers = string.ascii_lowercase + string.digits
             uuid = ""
+            for i in range(32):
+                        uuid += random.choice(letters_numbers)
             serializer.save(uuid=uuid)
             return Response(serializer.data)
+        return Response(serializer.errors)
