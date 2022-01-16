0c535a03ec0ba2f6acb2cec6b03002b0926b8fcf
Anthony <tonyudeagbala@gmail.com>
2022-01-16 09:50:09 +0000

    Create CustomerSerializer

---
+++ drf_uuid/serializers.py
@@ -0,0 +1,12 @@
+from attr import field
+from django.forms import models
+from rest_framework import serializers
+from .models import CowrywiseCustomer
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CowrywiseCustomer
+        fields = (
+            'uuid', 'created_at'
+        )
