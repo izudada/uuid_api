d90743ee68bbccdc4acaed6e3b726af5ed9dc143
Anthony <tonyudeagbala@gmail.com>
2022-01-16 15:13:30 +0000

    Edit model with ordering

--- drf_uuid/models.py
+++ drf_uuid/models.py
@@ -10,5 +10,8 @@ class CowrywiseCustomer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ('-created_at',)
+
     def __str__(self):
         return self.name
